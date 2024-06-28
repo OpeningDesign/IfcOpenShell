17efa5d9347ca59b853bc29393ba2f6b4b58e19d
Dion Moult <dion@thinkmoult.com>
2024-06-28 22:44:19 +1000

    Remove unload project button - superseded by Convert To Blender File operator

--- src/blenderbim/blenderbim/bim/module/project/ui.py
+++ src/blenderbim/blenderbim/bim/module/project/ui.py
@@ -160,7 +160,6 @@ class BIM_PT_project(Panel):
 
         row = self.layout.row(align=True)
         row.operator("bim.load_project_elements")
-        row.operator("bim.unload_project", text="", icon="CANCEL")
 
     def draw_project_ui(self, context):
         props = context.scene.BIMProperties
